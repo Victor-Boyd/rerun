bf605d822cc2c81af2e4f6d10133fcec6512dea3
Clement Rey <cr.rey.clement@gmail.com>
2024-04-30 11:03:06 +0200

    Sunset MeshProperties, introduce UVector3D and friends

--- rerun_py/rerun_sdk/rerun/datatypes/__init__.py
+++ rerun_py/rerun_sdk/rerun/datatypes/__init__.py
@@ -34,13 +34,6 @@ from .keypoint_pair import KeypointPair, KeypointPairArrayLike, KeypointPairBatc
 from .mat3x3 import Mat3x3, Mat3x3ArrayLike, Mat3x3Batch, Mat3x3Like, Mat3x3Type
 from .mat4x4 import Mat4x4, Mat4x4ArrayLike, Mat4x4Batch, Mat4x4Like, Mat4x4Type
 from .material import Material, MaterialArrayLike, MaterialBatch, MaterialLike, MaterialType
-from .mesh_properties import (
-    MeshProperties,
-    MeshPropertiesArrayLike,
-    MeshPropertiesBatch,
-    MeshPropertiesLike,
-    MeshPropertiesType,
-)
 from .quaternion import Quaternion, QuaternionArrayLike, QuaternionBatch, QuaternionLike, QuaternionType
 from .range1d import Range1D, Range1DArrayLike, Range1DBatch, Range1DLike, Range1DType
 from .rgba32 import Rgba32, Rgba32ArrayLike, Rgba32Batch, Rgba32Like, Rgba32Type
@@ -160,11 +153,6 @@ __all__ = [
     "MaterialBatch",
     "MaterialLike",
     "MaterialType",
-    "MeshProperties",
-    "MeshPropertiesArrayLike",
-    "MeshPropertiesBatch",
-    "MeshPropertiesLike",
-    "MeshPropertiesType",
     "Quaternion",
     "QuaternionArrayLike",
     "QuaternionBatch",
